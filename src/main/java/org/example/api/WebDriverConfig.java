310a5dba22958ca465b41967293e3db175ed2004
Artur Pagaev <paagaev@mail.ru>
2025-06-27 17:13:51 +0700

    Fix WebDriverConfig: add browser selection via system property, remove hardcoded ChromeDriver

--- src/main/java/org/example/api/WebDriverConfig.java
+++ src/main/java/org/example/api/WebDriverConfig.java
@@ -4,19 +4,28 @@ import org.openqa.selenium.WebDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 public class WebDriverConfig {
     public static final long WAIT_SEC_TIMEOUT = 10;
 
     public static WebDriver setDriver() {
-        WebDriverManager.chromedriver().driverVersion("138.0.7204.50").setup();
-        return new ChromeDriver();
+        String browser = System.getProperty("browser", "chrome").toLowerCase();
 
-        //Запуск тестов в Yandex Browser
-        //        System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver");
-        //        ChromeOptions options = new ChromeOptions();
-        //        options.setBinary("/Applications/Yandex.app/Contents/MacOS/Yandex");
-        //        return new ChromeDriver(options);
+        switch (browser) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                // Можно добавить настройки firefoxOptions
+                return new FirefoxDriver(firefoxOptions);
+
+            case "chrome":
+            default:
+                WebDriverManager.chromedriver().driverVersion("138.0.7204.50").setup();
+                ChromeOptions chromeOptions = new ChromeOptions();
+                // Можно добавить настройки chromeOptions
+                return new ChromeDriver(chromeOptions);
+        }
     }
 }
-
